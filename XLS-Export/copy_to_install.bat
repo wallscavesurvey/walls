5781a4d988242e78730727337f2db270a4848bb3
Andy Edwards <jedwards@fastmail.com>
2018-03-12 09:19:30 -0500

    fix XLS-Export build

--- XLS-Export/copy_to_install.bat
+++
@@ -1,4 +0,0 @@
-copy \work12\bin\Table2Shp.exe "\Table2Shp\Castile Karst\"
-copy \work12\bin\Table2Shp.exe "\Table2Shp\GNIS Database\"
-copy \work12\bin\Table2Shp.exe "\Table2Shp\Texas Historical Markers\"
-copy \work12\bin\Table2Shp.exe "\Table2Shp\Faunmap\"
